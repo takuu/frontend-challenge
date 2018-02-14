d9adbae4a8c8e9d3a07c823b7ba0fd5efec635b3
takuu <taku30@gmail.com>
2018-02-14 11:35:39 -0800

    Removing .idea from repo Updating tests Fix cross browser run script

--- src/containers/AppLayout/__tests__/AppLayout.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppLayout from '../AppLayout';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AppLayout />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
